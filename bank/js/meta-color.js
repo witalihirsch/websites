cbe0025e971ac8e619d166a9afe6f43b08c1bbd5
witalihirsch <witalihirsch1@gmail.com>
2023-03-15 15:03:55 +0600

    meta theme-color tests

--- bank/js/meta-color.js
+++ bank/js/meta-color.js
@@ -2,26 +2,25 @@ const motion = window.matchMedia("(prefers-reduced-motion: no-preference)");
 
 // Check if the media query matches or is not available.
 if (motion.matches) {
-  let lastKnownScrollPosition = 0;
-  let ticking = false;
-  
-  function changeColor(scrollPos) {
-    const saturation = scrollPos / (document.body.scrollHeight / 100);
-    const color = `hsl(24.3 ${saturation}% 54.2%)`;
-    document.body.style.background = color
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', color)
-  }
-
-  document.addEventListener('scroll', function(e) {
-    lastKnownScrollPosition = window.scrollY;
-
-    if (!ticking) {
-      window.requestAnimationFrame(function() {
-        changeColor(lastKnownScrollPosition)
-        ticking = false;
-      });
-
-      ticking = true;
+    let lastKnownScrollPosition = 0;
+    let ticking = false;
+    
+    function changeColor(scrollPos) {
+        const gradient = `linear-gradient(0deg, rgba(253, 207, 238, 1) 0%, rgba(175, 248, 255, 1) 50%, rgba(253, 207, 238, 1) 100%)`;
+        document.body.style.background = gradient;
+        document.querySelector('meta[name="theme-color"]').setAttribute('content', gradient);
     }
-  });
-} 
+
+    document.addEventListener('scroll', function(e) {
+        lastKnownScrollPosition = window.scrollY;
+
+        if (!ticking) {
+            window.requestAnimationFrame(function() {
+                changeColor(lastKnownScrollPosition)
+                ticking = false;
+            });
+
+            ticking = true;
+        }
+    });
+}
