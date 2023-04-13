307dfe8da2ff3fa59534caaa8e1fcece5a3f76bf
witalihirsch <witalihirsch1@gmail.com>
2023-04-13 13:24:54 +0600

    groceries remove hovers on touch devices

--- groceries/js/hover.js
+++
@@ -1,7 +0,0 @@
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-
-if (isTouchDevice) {
-    document.addEventListener('touchstart', function() {
-        document.querySelectorAll('*:hover').forEach(node => node.classList.remove('hover'));
-    });
-}
