7c52ceb38d299d7bb51147102a01c7a9d8987639
witalihirsch <witalihirsch1@gmail.com>
2023-04-17 20:04:33 +0600

    new categories, fixed burger menu, cart tooltip

--- groceries/js/burger.js
+++ groceries/js/burger.js
@@ -1,12 +1,15 @@
+var body = document.querySelector("body");
 const burger = document.querySelector(".item__burger");
 const header = document.querySelector(".header");
-const body = document.querySelector("body");
-const headerList = document.querySelector(".header__list");
-const itemSignUp = document.querySelector(".item__sign-up");
+const headerBurger = document.querySelector(".header__burger");
+const headerRow = document.querySelector(".header__row");
 
 burger.addEventListener("click", function() {
-	header.classList.toggle("clicked");
-	body.classList.toggle("lock");
-	headerList.classList.toggle("clicked");
-	itemSignUp.classList.toggle("clicked");
+    body.classList.toggle('clicked');
+    header.classList.toggle("clicked");
+    burger.classList.toggle("clicked");
+    headerBurger.classList.toggle("clicked");
+    setTimeout(function() {
+        headerRow.classList.toggle("clicked");
+    }, 150);
 });
--- groceries/js/buttons.js
+++ groceries/js/buttons.js
@@ -26,13 +26,34 @@ window.addEventListener('scroll', () => {
 const buttons = document.querySelectorAll('.item__btn');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (!button.classList.contains('item__btn-active')) {
-      buttons.forEach((btn) => {
-        btn.classList.remove('item__btn-active');
-      });
-      button.classList.add('item__btn-active');
-    }
-  });
+    button.addEventListener('click', () => {
+        if (!button.classList.contains('item__btn-active')) {
+            buttons.forEach((btn) => {
+                btn.classList.remove('item__btn-active');
+            });
+            button.classList.add('item__btn-active');
+        }
+    });
 });
 
+
+
+const inputAdd = document.querySelectorAll('.input__add');
+const tooltip = document.querySelector('.item__tooltip');
+const itemCart = document.getElementById('item__cart-img');
+
+function handleClick() {
+    tooltip.classList.add('item__tooltip--show');
+    setTimeout(() => {
+        tooltip.classList.remove('item__tooltip--show');
+    }, 3000);
+
+    inputAdd.forEach(inputAdd => {
+        inputAdd.removeEventListener('click', handleClick);
+        itemCart.setAttribute("src", "img/icon/cart-full.svg");
+    });
+}
+
+inputAdd.forEach(inputAdd => {
+    inputAdd.addEventListener('click', handleClick);
+});
